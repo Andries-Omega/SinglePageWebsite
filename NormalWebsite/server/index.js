09c49de7ef8c0bbab4a7d0f3ae2ff8e67c958fc3
Andries-Omega <achimule@jhb.dvt.co.za>
2023-06-25 17:56:21 +0200

    Normal website done

--- NormalWebsite/server/index.js
+++ NormalWebsite/server/index.js
@@ -1,7 +1,34 @@
 import { createServer } from 'http'
+import { readFile } from 'fs'
+const Port = 3040;
 
 createServer((req, res) => {
-
-}).listen(process.env.PORT, process.env.HOST_NAME, null, () => {
-    console.log(`Non single page website on http://localhost:${process.env.PORT}`)
+   switch(req.url){
+    case "/":
+        loadPage(res, "./pages/home.html")
+        return;
+    case "/home":
+        loadPage(res, "./pages/home.html")
+        return;
+    case "/second-page":
+        loadPage(res, "./pages/second-page.html")
+        return;
+    default:
+        loadPage(res, "./pages/not-found.html")
+        return
+   }
+}).listen(Port, "localhost", null, () => {
+    console.log(`Non single page website on http://localhost:${Port}`)
 })
+
+function loadPage(res, pageLocation) {
+    readFile(pageLocation, (err, data) => {
+        if(err){
+            res.end('Something went wrong')
+        }
+
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data)
+        res.end()
+    })
+}
